9ca905d0b9dbb88c0d870e8d6e962ea69afe71b6
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-01-11 15:23:31 -0800

    Add first level

--- RootScene.gd
+++ RootScene.gd
@@ -1,17 +1,12 @@
-extends Control
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    load_custom_cursor()
+
+func load_custom_cursor():
     var tex = ImageTexture.new()
     var img = Image.new()
     img.load("assets/art/cursor.png")
     tex.create_from_image(img)
     Input.set_custom_mouse_cursor(tex)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
--- StartMenu.gd
+++ StartMenu.gd
@@ -4,11 +4,7 @@ var current_x = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var tex = ImageTexture.new()
-    var img = Image.new()
-    img.load("assets/art/cursor.png")
-    tex.create_from_image(img)
-    Input.set_custom_mouse_cursor(tex)
+    pass
     
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +13,7 @@ func _process(delta):
     current_x = current_x + 0.01
     $StationSmall.rotation = $StationSmall.rotation + rot_speed * delta
     $StationSmall.translate(Vector2(0, 0.1*sin(current_x)))
+    
+
+func _on_StartButton_pressed():
+    get_tree().change_scene("levels/TestLevel.tscn")
