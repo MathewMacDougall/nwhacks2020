02ba2a4584917266d85c713b923c4f9b39dcf6e7
Mathew MacDougall <mmacd97@gmail.com>
2020-01-12 03:42:53 -0800

    added angular velocity to obstacle generator

--- scenes/ObjectGeneratorScene.gd
+++ scenes/ObjectGeneratorScene.gd
@@ -3,8 +3,10 @@ extends Node2D
 export (PackedScene) var object
 
 export var direction = Vector2(1, 1)
-export var min_speed = 10
-export var max_speed = 500
+export var min_linear_speed = 10
+export var max_linear_speed = 500
+export var min_angular_speed_deg = -360
+export var max_angular_speed_deg = 360
 export var interval_seconds = 1.0
 
 # Called when the node enters the scene tree for the first time.
@@ -15,5 +17,8 @@ func _ready():
 func _on_SpawnTimer_timeout():
     var o = object.instance()
     add_child(o)
-    var magnitude = rand_range(min_speed, max_speed)
-    o.linear_velocity = direction.normalized() * magnitude
+    var linear_magnitude = rand_range(min_linear_speed, max_linear_speed)
+    var angular_magnitude = rand_range(min_angular_speed_deg, max_angular_speed_deg)
+    o.linear_velocity = direction.normalized() * linear_magnitude
+    o.angular_velocity = deg2rad(angular_magnitude)
+    
