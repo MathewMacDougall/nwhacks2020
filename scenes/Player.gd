92fc5a870f8e3218dd748645ca72792716fc05dd
Gareth Ellis <gareth.ellis0@gmail.com>
2020-01-12 00:19:49 -0800

    Implemented Wall Grabbing With Joints

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,7 +1,5 @@
 extends RigidBody2D
 
-export(NodePath) onready var tilemap = get_node(tilemap)
-
 # Max speed of the player (pixels/sec)
 export var speed = 400
 
@@ -20,20 +18,18 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    
-    player_holding_joint = PinJoint2D.new()
-    player_holding_joint.set_name("ladder_joint")
-
-    var player = get_parent().get_node("Player")
-    var ladders = get_parent().get_node("Ladders")
-    player_holding_joint.softness = 0
-    player_holding_joint.set_node_a("../Player")
-    player_holding_joint.set_node_b("../Ladders")
-    
-#    add_child(player_holding_joint)
-    get_parent().call_deferred("add_child", player_holding_joint)
-  
-    assert(tilemap != null)
+    pass
+#    player_holding_joint = PinJoint2D.new()
+#    player_holding_joint.set_name("ladder_joint")
+#
+#    var player = get_parent().get_node("Player")
+#    var ladders = get_parent().get_node("Ladders")
+#    player_holding_joint.softness = 0
+#    player_holding_joint.set_node_a("../Player")
+#    player_holding_joint.set_node_b("../Ladders")
+#
+##    add_child(player_holding_joint)
+#    get_parent().call_deferred("add_child", player_holding_joint)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -53,37 +49,24 @@ func _process(delta):
 # This is called on mouse/keyboard events
 func _input(event):
     if event is InputEventMouseButton:
-        if on_a_wall:
+        if player_holding_joint:
+            player_holding_joint.free()
+            player_holding_joint = false
             # Let the player jump in the direction of the click
-            desired_jump_direction = event.position - position
+        # TODO: indent this line by 1
+        desired_jump_direction = event.position - position
         
 func _integrate_forces(state): 
     for i in range(get_colliding_bodies().size()):
         var collision_position = state.get_contact_local_position(i)
         var colliding_body = get_colliding_bodies()[i]
-        var tile_pos = tilemap.world_to_map(collision_position/2)
-        var tile_id = tilemap.get_cellv(tile_pos)
-        var tile_name = tilemap.get_tileset().tile_get_name(tile_id)
-        if colliding_body.name == "Ladders":
-#            player_holding_joint = PinJoint2D.new()
-#            player_holding_joint.position = collision_position
-#            player_holding_joint.softness = 0
-#            player_holding_joint.set_node_a(colliding_body.get_path())
-#            player_holding_joint.set_node_b(self.get_path())
-            pass;
-#        if tile_name == "ladder":
-#            # Lock the player to the object we collided with
-#            player_holding_joint = PinJoint2D.new()
-#            player_holding_joint.position = collision_position
-##            player_holding_joint.softness = 0
-#            player_holding_joint.set_node_a(colliding_body.get_path())
-#            player_holding_joint.set_node_b(self.get_path())
-#            pass
-            # Cancel out any velocity in the direction of the ladder
-#            var direction_of_collision = collision_position - position
-#            var a= linear_velocity
-#            var angle = direction_of_collision.angle_to(velocity)
-#            pass;
-                
         
-    
+        if colliding_body is Ladder && !player_holding_joint:
+            player_holding_joint = PinJoint2D.new()
+            player_holding_joint.disable_collision = false
+            player_holding_joint.position = collision_position
+            player_holding_joint.set_name("player_holding_joint")
+            player_holding_joint.softness = 0
+            player_holding_joint.set_node_a("../" + get_parent().get_path_to(self))
+            player_holding_joint.set_node_b("../" + get_parent().get_path_to(colliding_body))
+            get_parent().call_deferred("add_child", player_holding_joint)
