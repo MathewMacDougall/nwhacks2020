e7ec756bc2929067b675fa7eb1f4aef7875f2c06
Gareth Ellis <gareth.ellis0@gmail.com>
2020-01-12 01:48:23 -0800

    crawling prototype

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -8,12 +8,14 @@ export var speed = 400
 # The direction the player wants to jump in
 var desired_jump_direction = false
 
-# TODO: delete this
-# Whether or not this player is currently on a wall
-var on_a_wall = true
-
 # The joint holding the player to something
 var player_holding_joint = false
+
+# The max crawl speed (ie. if the player is crawling on a wall)
+var max_abs_crawl_speed = 100
+
+# The current crawl speed
+var current_crawl_speed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +30,17 @@ func _process(delta):
         linear_velocity = desired_jump_direction.normalized() * speed
         desired_jump_direction = false
         
+    # Crawl if we're on a wall 
+    if player_holding_joint and abs(current_crawl_speed) > 0:
+        # We need to remove the joint to update the position
+        var joint_player_path = player_holding_joint.node_a
+        player_holding_joint.node_a = ""
+        var wall_to_crawl_along = player_holding_joint.get_node(player_holding_joint.node_b)
+        var wall_rotation = wall_to_crawl_along.rotation
+        var position_update_vector = Vector2(cos(wall_rotation), sin(wall_rotation))
+        position += position_update_vector.normalized() * current_crawl_speed * delta
+        player_holding_joint.node_a = joint_player_path
+        
     # Determine sprite to draw
     if linear_velocity.x < 0:
         $Sprite.frame = 9
@@ -37,12 +50,25 @@ func _process(delta):
 # This is called on mouse/keyboard events
 func _input(event):
     if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+        # If we're on a wall, remove the joint holding us there and jump!
         if player_holding_joint:
             player_holding_joint.free()
             player_holding_joint = false
             # Let the player jump in the direction of the click
         # TODO: indent this line by 1
         desired_jump_direction = get_global_mouse_position() - position
+        
+    # Handle crawling if we're on a wall
+    if player_holding_joint:
+        if Input.is_action_pressed("ui_up"):
+            current_crawl_speed = max_abs_crawl_speed
+        elif Input.is_action_pressed("ui_down"):
+            current_crawl_speed = -max_abs_crawl_speed
+        else:
+            current_crawl_speed = 0
+    else:
+        current_crawl_speed = 0
+        
         
 func _integrate_forces(state): 
     for i in range(get_colliding_bodies().size()):
