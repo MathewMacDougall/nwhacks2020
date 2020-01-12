8de210933b75eed2f057399e5bfe774942064f2f
Gareth Ellis <gareth.ellis0@gmail.com>
2020-01-12 08:54:23 -0800

    Final Boss Kinda Done

---
+++ scenes/StaticBody2D.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+onready var animation_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    animation_player.play("Rotate")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
